427f4925186ad5ac5f8d91cf52eefc9f9efdc31b
Tormod Bjørnhaug <tormod@sinepost.no>
2016-10-28 19:46:57 +0200

    Added an abstraction layer to prepare for comparing measurements(moving window)

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys
+import paho.mqtt.publish as publish
+from json import JSONEncoder
+from monitor import monitor
+
+
+def readData():
+    f = open("valueStore")
+    if f.read()=='':
+        return None
+    data = json.load(f)
+    return data
+
+
+def writeData(data):
+    f = open("valueStore","w")
+    json.dumps(data,f)
+
+
+def reorder(data):
+    tempStore = {}
+    for subset in data:
+        tempStore[subset['host']]=subset
+    return tempStore
+
+
+def sendToLog(data):
+    publish.single("paradise/api/monitor",JSONEncoder().encode(data), port=8883, tls={'ca_certs':"ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+    publish.single("paradise/log/monitor","Alive=True", port=8883, tls={'ca_certs':"ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+
+if __name__ == "__main__":
+    m = monitor(sys.argv[1],sys.argv[2])
+    rawData = m.update()
+    processedData = reorder(rawData)
+    sendToLog(processedData)
